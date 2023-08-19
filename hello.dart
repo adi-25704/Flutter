6f439e064d3fdb1a385c18549261cce3d84121f5
Sagar-Krish1305 <202201139@daiict.ac.in>
2023-08-19 21:03:20 +0530

    Checking krish Branch

--- hello.dart
+++ hello.dart
@@ -1,30 +1,26 @@
 void main(List<String> args) {
   getHeavy();
+  
 }
 Future<int> heavy(int a){
   return Future.delayed(const Duration(seconds: 3),() {
     return a*2;
   });
 }
-Stream<String> getName(){
+Stream<String> getName(){  
   return Stream.periodic(const Duration(seconds: 1),(val){ 
     return 'Hello';
   });
 }
 void getHeavy() async{
+  var count = 0;
   await for (final value in getName()){
+    if(count == 10){
+      return;
+    }
     print(value);
+    print(count++);
+    
   }
 }
 
-class Pair<T1,T2>{
-  final T1 first;
-  final T2 second;
-  Pair(this.first, this.second);
-}
-// void answer(){
-//   String? name = null;
-//   String? surname = 'Krish';
-//   print(name??surname); // First Non Null Value
-
-// }
