09aef702b69e5431087e02f7c2f75f967ab2854a
Your Name <you@example.com>
2023-08-26 15:10:39 +0530

    removed comment

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -16,22 +16,3 @@ class MyApp extends StatelessWidget {
     return Homepage();
   }
 }
-// class hospital_login extends StatelessWidget{
-//   @override
-//   Widget build(BuildContext context){
-//     return Scaffold(
-//       appBar: AppBar(
-//         centerTitle: true,
-//         title: Text("login"),
-//       ),
-//       body: Center(
-//         child: ElevatedButton(
-//           onPressed: () {
-//             Navigator.push(context, MaterialPageRoute(builder: (context) => null));
-//           },
-//           child: Text("go back"),
-//         ),
-//       ),
-//     );
-//   }
-// }
